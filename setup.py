3592649aead24344d2fd5ca40463526b4b1d40ec
Pouya Motakef <p.motakef@gmail.com>
2023-09-08 14:43:59 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+dependencies = [
+
+]
+
+
+setup(
+    name='symbolipy',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/pmotakef/symbolipy',
+    license='BSD 2-Clause "Simplified"',
+    author='Pouya Motakef',
+    author_email='p.motakef@gmail.com',
+    description='Present mathematical equality and inequality in symbol format.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=dependencies,
+)
